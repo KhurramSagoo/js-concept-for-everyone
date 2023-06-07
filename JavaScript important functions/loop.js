29249a8fe639b4ed6930001f414218e446372bc6
KhurramSagoo <uaf.khurram@gmail.com>
2023-06-07 14:44:08 +0500

    for loop, call back

---
+++ JavaScript important functions/loop.js
@@ -0,0 +1,16 @@
+for(let x=10; x>27;x--){
+  console.log(x)
+}
+let forloop=(console.log(x))
+// 0,1,2,3,4,5,6,7,8,9
+const outputElement = document.getElementById("for");
+document.body.innerHTML=forloop;
+console.log("========")
+
+let i = 0;
+while (i < 5) {
+  i++;
+  console.log(i);
+}
+
+// 1,2,3,4,5
--- script.js
+++ script.js
@@ -1,74 +1,18 @@
-//destructuring
-arr=[1,3,4,5,7]
+function utopianTree(n) {
+  let height = 1;
 
-function avg(a,b,c,d,e){
-  return a+b+c+d+e/5
+  for (let i = 1; i <= n; i++) {
+    if (i % 2!== 0) {
+      // Spring season: height doubles
+      height *= 2;
+    } else {
+      // Summer season: height increases by 1
+      height += 1;
+    }
+  }
+
+  return height;
 }
 
-let a=avg(...arr)
-console.log(a);
-
-
-b=5;
-console.log(b)
-
-
-obj1={
-    name1:"khurram",
-    class1:"8a",
-    language:"js"
-}
-
-console.log(obj1)
-
-let obj2={...obj1,language:"python"}
-console.log(obj2)
-
-
-let {name1,class1,language}=obj1;
-console.log(name1,class1,language)
-
-//async await
-
-console.log("this that is good")
-
-setTimeout(()=>{
-    console.log("this that from async await")
-},3000)
-
-console.log("this that is bad")
-
-const { isUtf8 } = require("buffer");
-//promises and async await
-
-//this fs will be console later
-// let fs= require("fs");
-// fs.readFile("file.txt","utf-8",(err,data)=>{
-//   console.log(err,data)
-// })
-
-// //this will called earlier
-// console.log("this is end of file")
-
-
-//for promise with respect to fs
-let fs=require("fs/promises");
-const readThree=(file1,file2,file3)=>{
-  let a6=fs.readFile(file1,"utf-8");
-  let a4=fs.readFile(file2,"utf-8");
-  let a5=fs.readFile(file3,"utf-8");
-  let c1=await a6;
-}
-let c=fs.readFile("file.txt","utf-8");
-
-a.then((c) => {
-  console.log(c)
-  
-}).catch((err) => {
-  
-});
-// console.log(c)
-
-console.log("this is mid");
-console.log("this is end of file");
-
+const N = [3, 0, 1, 4];
+function utopianTree(n);
